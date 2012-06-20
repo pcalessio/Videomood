49b684386cba3bb2d995acbb667ff0dc6a9acd88
alessio <alessio@192.168.0.3>
2012-06-21 01:05:54 +0200

    Insert a new video and retrive video now work

--- VideoMoodServer/VideoMoodApp/models.py
+++ VideoMoodServer/VideoMoodApp/models.py
@@ -21,26 +21,50 @@ class Mood(models.Model):
 class TagRelation(models.Model):
     video = models.ForeignKey(Video)
     mood = models.ForeignKey(Mood)
-    num_tag = models.IntegerField()
+    num_tag = models.IntegerField(default=0)
 
-
+    def __unicode__(self):  
+        return str(self.video) + ', ' + str(self.mood) + ', ' + str(self.num_tag)    
+        
+    class Meta:
+        unique_together = ('video', 'mood')
+        
 class Utils(): 
 
     @staticmethod
-    def insertNewVideo(video_id, category, mood_name, mood_input):
-        video = Video.objects.create(video_id=video_id, category=category)
-        mood = Mood.objects.create(mood_name=mood_name, popularity=None )
-        Utils.insertNewVideo2(video, mood)
-        
+    def insertNewVideo(video_id, mood_name, category=''):
+        video, created = Video.objects.get_or_create(video_id=video_id)
+        mood, created2 = Mood.objects.get_or_create(mood_name=mood_name)
+        Utils._insertNewVideo(video, mood)
+        video.category=category
+        video.save()
+    
     @staticmethod
-    def insertNewVideo2(video_input, mood_input):
-        #check if the relation video-mood exists already
-        numTags=0
-        tr = TagRelation.objects.get(video=video_input, mood=mood_input)
-        if tr:
-            numTags = tr.num_tags
-        numTags = numTags+1
-        #insert
-        m1 = TagRelation(video=video_input, mood=mood_input, num_tag=numTags)
-        m1.save()
+    def _insertNewVideo(video_input, mood_input):
+        tr, created = TagRelation.objects.get_or_create(video_id=video_input, mood=mood_input)
+        tr.num_tag = tr.num_tag + 1
+        tr.save(); 
+    
+    @staticmethod
+    def getVideosFromMood(mood_name):
+        v = Video.objects.filter(mood=mood_name)
+        return v    
+
+
+#    @staticmethod
+#    def insertNewVideo2(video_input, mood_input):
+#        #check if the relation video-mood exists already
+#        #vid = Video.objects.filter(video_id=video_input.video_id, mood=mood_input)
+#        vid = TagRelation.objects.filter(video_id=video_input, mood=mood_input)
+#        if vid:
+#            tr = TagRelation.objects.get(video=video_input, mood=mood_input)
+#            tr.num_tag = tr.num_tag + 1
+#            tr.save()
+#        else:
+#            #insert
+#            m1 = TagRelation(video=video_input, mood=mood_input, num_tag=1)
+#            m1.save()
+    
+    
+    
     
