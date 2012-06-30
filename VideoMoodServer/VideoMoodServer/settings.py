55dda232d3bc506aa3614bc2cbfc24ddff9b8338
alessio <alessio@192.168.0.3>
2012-06-30 23:39:47 +0200

    dd

--- VideoMoodServer/VideoMoodServer/settings.py
+++ VideoMoodServer/VideoMoodServer/settings.py
@@ -117,7 +117,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/alessio/git/videomood/VideoMoodServer/template/',
+    #'/Users/alessio/git/videomood/VideoMoodServer/template/',
     os.path.join(ROOT_PATH, '../template/'),
 )
 
